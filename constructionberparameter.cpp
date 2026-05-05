d16bf024aad602d4ab7d132c531c6be5e3592bfc
pasyaa <mhmdpsyh@gmail.com>
2026-05-05 17:21:19 +0700

    cout << "Constructor Terpanggil" <<

--- constructionberparameter.cpp
+++ constructionberparameter.cpp
@@ -4,13 +4,9 @@ using namespace std;
 class mahasiswa {
 public:
     mahasiswa(); // Constructor
+    mahasiswa(int nim, string nama); // Constructor dengan parameter
     };
 
 mahasiswa::mahasiswa() {
     cout << "Constructor Terpanggil" << endl; // Definisi Constructor
-};
-
-int main() {
-    mahasiswa mhs; // Pembuatan Object
-    return 0;
 }

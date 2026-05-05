a7ba5b28b0457b424b5e564ddfcfb3c85f7c2400
pasyaa <mhmdpsyh@gmail.com>
2026-05-05 16:58:14 +0700

    static int getNim(){

--- staticfunction.cpp
+++ staticfunction.cpp
@@ -9,5 +9,16 @@ class Mahasiswa {
 
     public:
     int id;
-    
+    string nama;
+
+    void setID();
+    void printAll();
+
+    static void setNim(int pnim){
+        nim = pnim;
+    }
+
+    static int getNim(){
+        
+    }
 }
